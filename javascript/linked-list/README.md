b60153886cca6fd2096ac3ec31f19a9baa1a4630
rubabanat <rubabanat1996@gmail.com>
2021-04-03 17:55:49 +0300

    linked list

---
+++ javascript/linked-list/README.md
@@ -0,0 +1,35 @@
+# Singly Linked List
+
+
+## Challenge Summary
+
+Need to create a node and link list constructor class along with some methods 
+
+
+## Challenge Description
+
+Create a linked list class that has methods to append, insert before, and includes nodes and toString method .
+
+## Approach & Efficiency
+
+The Big O 
+
+space : O(1)
+Time : O(1)
+
+---
+
+## API
+
+3 Methods 
+1- Insert Function --> which takes any value as an argument and adds a new node with that value to the head of the list .
+
+2- Includes Function -->  which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
+
+3- ToString Function --> which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
+"{ a } -> { b } -> { c } -> NULL"
+
+---
+
+# THE END
+
--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,71 @@
 'use strict';
 
-// Require our linked list implementation
-const LinkedLilst = require('../index');
+const Node = require('../linked-list').Node;
+const LinkedList = require('../linked-list').LinkedList;
 
-describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
-  });
+
+// test('init node class', () => {
+//     let value = 5;
+//     let node = new Node(value);
+//     expect(node.val).toEqual(5);
+//     expect(node.next).toEqual(null);
+// });
+
+test('init LinkedList class', () => {
+    let linkedList = new LinkedList();
+    expect(linkedList.head).toEqual(null);
 });
+
+test('ThE first node should be the head', () =>{
+    let ll = new LinkedList();
+    ll.insert(6);
+    ll.insert(9);
+
+    expect(ll.head.val).toEqual(9);
+});
+
+test('should be able to add new node to empty linked list', () => {
+  
+    let ll = new LinkedList();// empty
+    let value = 7;
+    ll.insert(value);
+
+    expect(ll.head.val).toEqual(value);
+    expect(ll.head.next).toBeNull();
+});
+
+test('should be able to add new header to linked list', () => {
+
+    let ll = new LinkedList();// empty
+    ll.insert(100);
+    let value = 7;
+    ll.insert(value);
+
+    expect(ll.head.val).toEqual(value);
+    expect(ll.head.next.val).toEqual(100);
+});
+
+test('Should be able to insert multiple Nodes' , () =>{
+    let ll = new LinkedList();
+    ll.insert(100);
+    ll.insert(200);
+
+    expect(ll.length).toEqual(2);
+});
+
+test('Should return true when finding a value else return false' , () =>{
+    let ll = new LinkedList();
+    ll.insert(200);
+   
+    expect(ll.includes(200)).toEqual(true);
+    expect(ll.includes(500)).toEqual(false);
+});
+
+test('Should be able to return all values in LL as a string' , ()=>{
+    let ll = new LinkedList();
+    ll.insert(100);
+    ll.insert(200);
+    
+    expect(ll.toString()).toEqual("{200} -> {100} -> NULL");
+});
+
